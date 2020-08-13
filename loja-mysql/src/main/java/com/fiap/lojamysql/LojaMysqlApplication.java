74522c5b708a365016e256b8ec66051564e0f68c
Daniel Kchevi <dkchevi@hotmail.com>
2020-08-12 22:44:20 -0300

    estrutura base api loja

---
+++ loja-mysql/src/main/java/com/fiap/lojamysql/LojaMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.lojamysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaMysqlApplication.class, args);
+	}
+
+}
---
+++ loja-mysql/src/test/java/com/fiap/lojamysql/LojaMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.lojamysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
